c7b473646964440b405f553d3ee0709cfa97b439
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-12 12:56:12 +0300

    Update cnc_01.cpp

--- basic/cnc_01.cpp
+++ basic/cnc_01.cpp
@@ -1,49 +1,38 @@
 #include <concepts>
 #include <iostream>
 
-template <typename T> 
+template <typename T>
 requires std::integral<T>  //requires clause
-T gcd1(T a, T b)
+T sum1(T a, T b)
 {
-    if (b == 0)
-        return a;
-
-    return gcd1(b, a % b);
+    return a + b;
 }
 
 
 template<std::integral T> //constrained template parameter
-T gcd2(T a, T b)
+T sum2(T a, T b)
 {
-    if (b == 0)
-        return a;
-
-    return gcd2(b, a % b);
+    return a + b;
 }
 
 //abbreviated (constrained) template syntax
-std::integral auto gcd3(std::integral auto a, std::integral auto b)
+std::integral auto 
+sum3(std::integral auto a, std::integral auto b)
 {
-    if (b == 0)
-        return a;
-
-    return gcd3(b, a % b);
+    return a + b;
 }
 
 //abbreviated (unconstrained) template syntax
-auto gcd4(auto a, auto b)
+auto sum4(auto a, auto b)
 {
-    if (b == 0)
-        return a;
-
-    return gcd4(b, a % b);
+    return a + b;
 }
 
 int main()
 {
 
-    std::cout << "gcd1(40, 28)= " << gcd1(40, 28) << '\n';
-    std::cout << "gcd2(40, 28)= " << gcd2(40, 28) << '\n';
-    std::cout << "gcd3(40, 28)= " << gcd3(40, 28) << '\n';
-    std::cout << "gcd4(40, 28)= " << gcd4(40, 28) << '\n';
+    std::cout << "sum(40, 28)= " << sum1(40, 28) << '\n';
+    std::cout << "sum(40, 28)= " << sum2(40, 28) << '\n';
+    std::cout << "sum(40, 28)= " << sum3(40, 28) << '\n';
+    std::cout << "sum(40, 28)= " << sum4(40, 28) << '\n';
 }
