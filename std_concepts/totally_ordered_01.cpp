bf84f91665bf44d743ccd787d245c388f29e40f8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-12 13:58:31 +0300

    Update and rename std_totally_ordered_1.cpp to totally_ordered_01.cpp

--- std_concepts/totally_ordered_01.cpp
+++ std_concepts/totally_ordered_01.cpp
@@ -3,21 +3,20 @@
 
 void foo(std::totally_ordered auto x);
 
-
 struct Nec {};
 struct Erg {
 	auto operator<=>(const Erg& rhs) const = default;
 };
 
 struct Bar {
-	bool operator==(const Bar &)const;
+	bool operator==(const Bar&)const;
 };
 
 int main()
 {
 	foo(1);
-	foo(std::string{"remzi"});
+	foo(std::string{ "remzi" });
 	//foo(Nec{}); //invalid
-	foo(Erg{}); 
+	foo(Erg{});
 	//foo(Bar{}); 
 }
