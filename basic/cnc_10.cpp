f089e6980914e929cc883835ca7f995a21d11387
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-26 12:15:22 +0300

    Create cnc_10.cpp

---
+++ basic/cnc_10.cpp
@@ -0,0 +1,26 @@
+
+#include <concepts>
+#include <string>
+
+template <typename T, std::convertible_to<T> auto val>
+class Myclass {
+
+};
+
+struct A {
+	operator std::string()const;
+};
+
+struct B {};
+
+
+char name[] = "necati";
+
+int main()
+{
+	Myclass<std::string, name> m1;
+	Myclass<std::string, A{}> m2;
+	//Myclass<std::string, B{}> m3;	  // error
+}
+
+
