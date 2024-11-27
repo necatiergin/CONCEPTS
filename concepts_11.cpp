a13ca64520ac910d88c84bba8edf42a8ac8f91f4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-27 17:03:51 +0300

    Create concepts_11.cpp

---
+++ concepts_11.cpp
@@ -0,0 +1,29 @@
+#include <concepts>
+
+template <typename T>
+concept Clock = requires (T clock)
+{
+	clock.start();
+	clock.stop();
+	clock.time();
+};
+
+
+struct Myclock {
+	void start();
+	void stop();
+	double time()const;
+};
+
+template <Clock C>
+class Myclass {
+
+};
+
+
+
+int main()
+{
+	Myclass<Myclock> m;
+	//...
+}
