c430dda170cdeb1c05b8bff33759461dac83399a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-28 00:44:44 +0300

    Create examples_05.cpp

---
+++ examples_05.cpp
@@ -0,0 +1,16 @@
+#include <concepts>
+
+template <typename F>
+concept invoke_integral = requires (F f) {
+    { f() } -> std::integral;
+};
+
+template <invoke_integral F>
+void foo(F f) {};
+
+int main() 
+{
+    foo([]() { return 42; }); // valid
+    foo([]() { return 4.3; }); // invalid return type is notintegral
+    foo(42); // 42() is not valid
+}
