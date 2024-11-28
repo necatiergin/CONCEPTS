e18a084fe635d98d3e85a31385f61834924a9c6d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-28 19:19:04 +0300

    Update concepts_05.cpp

--- concepts_05.cpp
+++ concepts_05.cpp
@@ -6,12 +6,12 @@ concept Pointer = std::is_pointer_v<T>;
 template <typename T, typename U>
 requires Pointer<T> && (!Pointer<U>) 
 class Foo {
-
+	//...
 };
 
 int main()
 {
 	Foo<int*, double> x1;
-	Foo<int, double> x2; //error
-	Foo<int* , double*> x3; //error
+	// Foo<int, double> x2; //error
+	// Foo<int* , double*> x3; //error
 }
