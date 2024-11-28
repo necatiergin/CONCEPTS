8d0fb15a0245a068bdd81ff06f3d38e18f45d033
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-28 07:16:49 +0300

    Create requires_expression_15.cpp

---
+++ requires_expression_15.cpp
@@ -0,0 +1,20 @@
+template<typename T, typename P>
+concept Con = requires(T tval, P p) {
+	*p;
+	*p < tval;
+	p == nullptr;
+};
+
+
+template <typename T, typename P>
+requires Con<T, P>
+class Myclass {
+	//...
+};
+
+int main()
+{
+	Myclass<int, int*> m1;
+	Myclass<int, int> m2; //invalid
+	//...
+}
