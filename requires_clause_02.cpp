0204b0a8990a2482a3b487d98e89f2dbbeda95e5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-07-28 17:28:25 +0300

    Update requires_clause_02.cpp

--- requires_clause_02.cpp
+++ requires_clause_02.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <ranges>
+#include <vector>
 
 template<typename T>
 class ValOrColl {
 	T value;
 public:
 	ValOrColl(const T& val) : value{ val } {}
-	ValOrColl(T&& val) : value{ std::move(val) } { }
-	void print() const 
+	ValOrColl(T&& val) : value{ std::move(val) } {}
+	void print() const
 	{
 		std::cout << value << '\n';
 	}
 
-	void print() const requires std::ranges::range<T> 
+	void print() const requires std::ranges::range<T>
 	{
 		for (const auto& elem : value) {
 			std::cout << elem << ' ';
@@ -21,3 +22,14 @@ public:
 	}
 	//...
 };
+
+int main()
+{
+	std::vector<int> ivec(5);
+	ValOrColl x(ivec);
+
+	x.print();
+
+	ValOrColl<int> y(10);
+	y.print();
+}
