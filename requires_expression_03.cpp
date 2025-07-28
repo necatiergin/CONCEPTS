6e833963fa9668f4b7cc7329b40d4b6f3eb84b0b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-07-28 16:19:40 +0300

    Update and rename requires_expression_17.cpp to requires_expression_03.cpp

--- requires_expression_03.cpp
+++ requires_expression_03.cpp
@@ -1,5 +1,5 @@
 template<typename Container>
-concept C1 = requires(Container::size_type) { }; //error
+concept C1 = requires(Container::size_type) { }; //error / empty body
 
 
 template<typename Container>
